61a45afe0b8efd0a517fce235b23c17815427a61
ecaoili24 <erika.caoili@gmail.com>
2020-01-04 19:56:19 +0000

    the header file

---
+++ monty.h
@@ -0,0 +1,52 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* helper functions */
+void parse(char *fileName, stack_t **head);
+void dropnl(char *src);
+void exe_operation(char *operation, stack_t **head, unsigned int line_number);
+/* commands */
+void do_push(stack_t **stack, unsigned int line_number);
+void do_pall(stack_t **stack, unsigned int line_number);
+void do_pint(stack_t **stack, unsigned int line_number);
+void do_pop(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
