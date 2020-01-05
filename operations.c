5dd9932562b9ed026848441a7f61c79771fa5a1e
ecaoili24 <erika.caoili@gmail.com>
2020-01-05 21:34:19 +0000

    added the sub opcode to the operatins file

---
+++ operations.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+
+/**
+ * add - Pops the top two items from the stack and pushes their sum
+ * @stack: Double pointer to the head of the stack
+ * @l: The line number currently being run
+ */
+void add(stack_t **stack, unsigned int l)
+{
+	int sum;
+
+	if (!*stack || !(*stack)->next)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", l);
+		cleanup();
+		exit(EXIT_FAILURE);
+	}
+
+	sum = (*stack)->n + (*stack)->next->n;
+	pop(stack, l);
+	pop(stack, l);
+	pushInt(sum);
+}
+
+/**
+ * sub - the opcode that subtracts the top element of the stack from the second
+ * top element of the stack.
+ * @stack: double pointer to the head of the stack
+ * @l: the line number currently being run
+ */
+void sub(stack_t **stack, unsigned int l)
+{
+	int x, y, difference;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n", l);
+		cleanup();
+		exit(EXIT_FAILURE);
+	}
+
+	x = (*stack)->n;
+	y = (*stack)->next->n;
+
+	difference = y - x;
+
+	pop(stack, l);
+	(*stack)->n = difference;
+}
