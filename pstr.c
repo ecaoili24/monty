85ddc6029573cd3611f40eb28ff818d61996e955
Ben Keener <keener4christ@gmail.com>
2020-01-06 00:09:03 +0000

    Added documentation

--- pstr.c
+++ pstr.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * pstr - Prints stack values as a string
+ * @stack: Double pointer to the top of the stack
+ * @l: The line number currently running
+ */
 void pstr(stack_t **stack, unsigned int l)
 {
 	stack_t *s = *stack;
